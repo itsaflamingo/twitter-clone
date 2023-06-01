1b62a6524bdc492893e91b7b7efb1e0f676dfb1e
Liz <elizabeth.pinero.r@gmail.com>
2023-06-01 14:56:12 -0400

    Styled signInPopup

--- src/components/Dashboard/SignInPopup.js
+++ src/components/Dashboard/SignInPopup.js
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { ShowSignInPopupContext } from "../contexts/signInPopupContext";
+import GoogleSignIn from "../Menu_and_Aside/GoogleSignIn";
 
 export default function SignInPopup() {
 
@@ -8,8 +9,10 @@ export default function SignInPopup() {
     return (
     <div className="sign-in-popup">
         <h4>Sorry, log in to gain full access</h4>
-        <button>Sign In</button>
-        <button onClick={() => setShowPopup(false)}>No, thanks</button>
+        <div className="popup-button-container">
+            <GoogleSignIn />
+            <button className="close-popup" onClick={() => setShowPopup(false)}>No, thanks</button>
+        </div>
     </div>
     )
 }
